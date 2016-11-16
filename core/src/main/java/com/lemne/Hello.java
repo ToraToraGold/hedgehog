8f2cb6063ccfe78bcb59ce101b16c1d197b10e53
Andrei Eremia <andrei.eremia@4finance.com>
2016-11-16 11:50:07 +0200

    hi there

---
+++ core/src/main/java/com/lemne/Hello.java
@@ -0,0 +1,11 @@
+package com.lemne;
+
+/**
+ * Created by aeremia on 11/16/2016.
+ */
+public class Hello {
+
+    public static void main (String[] args){
+        System.out.println("Hi");
+    }
+}
