c95bc5612f81427e40903a9767eda6859c05a5a5
Andrei Eremia <andrei.eremia@4finance.com>
2016-11-16 13:51:52 +0200

    added potato

---
+++ core/src/main/java/com/lemne/services/LogReaderServiceImpl.java
@@ -0,0 +1,12 @@
+package com.lemne.services;
+
+/**
+ * Created by aeremia on 11/16/2016.
+ */
+public class LogReaderServiceImpl implements LogReaderService {
+
+    @Override
+    public void OpenFile(){
+        System.out.print("hi, file opened");
+    }
+}
