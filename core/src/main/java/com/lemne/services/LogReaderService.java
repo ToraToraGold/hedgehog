79b1207d6459045c7e02c81279bfcbd6c7a19e3c
Andrei Eremia <andrei.eremia@4finance.com>
2016-11-16 13:50:57 +0200

    better hello

---
+++ core/src/main/java/com/lemne/services/LogReaderService.java
@@ -0,0 +1,9 @@
+package com.lemne.services;
+
+/**
+ * Created by aeremia on 11/16/2016.
+ */
+public interface LogReaderService {
+
+    void OpenFile();
+}
---
+++ needles/src/main/java/com/lemne/Hello.java
@@ -0,0 +1,15 @@
+package com.lemne;
+
+import com.lemne.services.LogReaderService;
+import com.lemne.services.LogReaderServiceImpl;
+
+/**
+ * Created by aeremia on 11/16/2016.
+ */
+public class Hello {
+
+    public static void main (String[] args){
+        LogReaderService logReaderService = new LogReaderServiceImpl();
+        logReaderService.OpenFile();
+    }
+}
